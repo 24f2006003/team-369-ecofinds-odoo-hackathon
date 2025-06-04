873bae1fec4224509883ca7651cf0e2c31380821
ArnavLabh <arnavlabh@gmail.com>
2025-06-04 10:37:47 +0530

    deploying on vercel

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from app import app
+from app import create_app
+
+app = create_app()
+
+# This is needed for Vercel
+app.config['SERVER_NAME'] = None
 
 if __name__ == '__main__':
     app.run(debug=True)
